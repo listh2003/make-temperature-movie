07bfd4622b821dcc640f1d126c241b0d0cf19bae
jprb-walton <jprb.walton@gmail.com>
2017-06-23 09:15:11 +0100

    Switch contouring to iplt from qplt to get full window, and save each plot into an image file.

--- plotTemperatures.py
+++ plotTemperatures.py
@@ -1,12 +1,15 @@
 """
 Read in dynamic surface temperature data and contour plot it.
 """
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
 import iris
 import iris.plot as iplt
 import iris.quickplot as qplt
 import iris.coord_categorisation as icat
-
-import matplotlib.pyplot as plt
 
 def main():
     # Read all the temperature values.
@@ -20,14 +23,21 @@ def main():
     for time in range(0, tmax):
 
        # Contour plot the temperatures and add the coastline.
-       qplt.contourf(temperatures[time], 15)
+       iplt.contourf(temperatures[time], 15)
        plt.gca().coastlines()
 
        # Extract the year value and display it (coordinates used are
        # those of the data).
        year = temperatures.coord('year')[time].points[0]
        plt.text(0, -60, year, horizontalalignment='center') 
-       iplt.show()
+       
+       # Now save the plot in an image file.
+       filename = "%s.png" % year
+       plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+       
+       # Discard the figure (otherwise the text will be overwritten
+       # by the next iteration).
+       plt.close()
 
 if __name__ == '__main__':
     main()
