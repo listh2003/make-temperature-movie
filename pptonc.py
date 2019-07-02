ebe494cdea030c80c705bdfbc80f4656322c7eb5
jprb-walton <jprb.walton@gmail.com>
2019-07-02 16:11:11 +0100

    Update to handly conversion of dailies.

--- pptonc.py
+++ pptonc.py
@@ -1,6 +1,6 @@
 import iris
 
-def main():
+def convert_montly():
    mon = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
    
    yr1 = 2040
@@ -17,7 +17,29 @@ def main():
        print ncfile
      
        iris.save(data, ncfile)
+
      
+def convert_daily():
+   
+   dirname = 'gpp_chlrphyl_1850/'
+   for month in range(1, 13):
+     for day in range(1, 31, 10):
+       name = 'bk179a.p61850{:02d}{:02d}'.format(month,day)
+       ppfile = dirname + name + '.pp'
+       
+       data = iris.load(ppfile)
+       
+       ncfile1 = dirname + 'chlrphyl/' + name + '.nc'
+       ncfile2 = dirname + 'gpp/' + name + '.nc'      
+       
+       iris.save(data[0], ncfile1)
+       iris.save(data[1], ncfile2) 
+
+
+def main():
+   convert_daily()
+   
+        
 if __name__ == '__main__':
     main()
 
