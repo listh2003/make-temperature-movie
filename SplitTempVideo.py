9ae8e7be3d81ae14023965ac2f021ac872d3c8fe
jprb-walton <jprb.walton@gmail.com>
2019-07-03 15:07:10 +0100

    Make image bigger (1200 by 600).

--- SplitTempVideo.py
+++ SplitTempVideo.py
@@ -11,6 +11,7 @@ import matplotlib
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 
 import numpy as np
 
@@ -57,8 +58,8 @@ def create_video():
     SpawnCommand("ffmpeg -i image-%04d.png TemperatureVideo1.mp4")
     SpawnCommand('ffmpeg -i TemperatureVideo1.mp4 -filter:v "setpts=5.0*PTS" ' + sys.argv[1] + '.mp4')
     print ("Deleting the unneeded images...")
-    SpawnCommand("rm -f *.png")
-    SpawnCommand("rm -f TemperatureVideo1.mp4")
+#    SpawnCommand("rm -f *.png")
+#    SpawnCommand("rm -f TemperatureVideo1.mp4")
     
 
 
@@ -123,18 +124,26 @@ def main():
 
     for time in range(minTime, maxTime):
 
+        # Set up for larger image.
+        fig = plt.figure(figsize=(6,3), dpi=200)
+        rect = 0,0,1200,600
+        fig.add_axes(rect)
+        geo_axes = plt.axes(projection=ccrs.PlateCarree())
+
+        # We need to fix the boundary of the figure (otherwise we get a black border at left & top).
+        # Cartopy removes matplotlib's axes.patch (which normally defines the boundary) and
+        # replaces it with outline_patch and background_patch.  It's the former which is causing
+        # the black border.  Get the axis object and make its outline patch invisible.
+        geo_axes.outline_patch.set_visible(False)
+        plt.margins(0,0)
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+
         # Contour plot the temperatures and add the coastline.
         
         iplt.contourf(anomaly[time], levels = (-6, -3, 0, 4, 8, 12, 17, 22, 28), colors = ('darkblue', 'blue', 'cyan', 'lightyellow', 'yellow', 'orange', 'darkorange', 'red'))
         #-6.4358826, 27.94899
         plt.gca().coastlines()
         #plt.colorbar(boundaries = (-6, -3, 0, 4, 8, 12, 16, 20, 25), values = (-6, -3, 0, 4, 8, 12, 16, 20))
-        # We need to fix the boundary of the figure (otherwise we get a black border at left & top).
-        # Cartopy removes matplotlib's axes.patch (which normally defines the boundary) and
-        # replaces it with outline_patch and background_patch.  It's the former which is causing
-        # the black border.  Get the axis object and make its outline patch invisible.
-        ax = plt.gca()
-        ax.outline_patch.set_visible(False)
 
         # Extract the year value and display it (coordinates used in locating the text are
         # those of the data).
@@ -147,7 +156,7 @@ def main():
         # Now save the plot in an image file.  The files are numbered sequentially, starting
         # from 000.png; this is so that the ffmpeg command can grok them.
         filename = "image-%04d.png" % time
-        plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+        plt.savefig(filename, dpi=200)
         
         # Discard the figure (otherwise the text will be overwritten
         # by the next iteration).
