d7df668c7f13e909ae9277a5d3a641d7f24d3b2a
jprb-walton <jprb.walton@gmail.com>
2019-07-08 15:28:16 +0100

    Make image bigger.

--- rainfall.py
+++ rainfall.py
@@ -6,16 +6,22 @@ from iris.experimental.equalise_cubes import equalise_attributes
 from spawnCommand import SpawnCommand
 import iris.plot as iplt
 import iris.coord_categorisation as icat
+import cartopy.crs as ccrs
 
 
 def create_video():
 
     #creating the video
-    SpawnCommand("ffmpeg -i rainfall/image-%04d.png rainfall/temp.mp4")
-    SpawnCommand('ffmpeg -i rainfall/temp.mp4 -filter:v "setpts=5.0*PTS" rainfall/rainfall.mp4')
+    SpawnCommand("rm -f rainfall/rainfall.avi")
+    options = ("-r 5 -vcodec png -y -i " 
+             + "rainfall/image-%04d.png -r 5 -vcodec msmpeg4v2 -qblur 0.01 -qscale 5 ")
+    SpawnCommand("ffmpeg " + options + "rainfall/rainfall.avi")
+
+#    SpawnCommand("ffmpeg -i rainfall/image-%04d.png rainfall/temp.mp4")
+#    SpawnCommand('ffmpeg -i rainfall/temp.mp4 -filter:v "setpts=5.0*PTS" rainfall/rainfall.mp4')
     print ("Deleting the unneeded images...")
 #    SpawnCommand("rm -f *.png")
-    SpawnCommand("rm -f rainfall/temp.mp4")
+#    SpawnCommand("rm -f rainfall/temp.mp4")
     
 
 def myload():
@@ -45,20 +51,27 @@ def main():
     
     for time in range(minTime, maxTime):
 
-        # Contour plot the temperatures and add the coastline.
-        
-        iplt.contourf(cube[time], levels = (0.000008, 0.00002, 0.00007, 0.0002, 0.001, 0.05), colors = ('cyan', 'blue', 'yellow', 'orange', 'red'))
-        
-        
-        #-6.4358826, 27.94899
-        plt.gca().coastlines()
-        #plt.colorbar(boundaries = (-6, -3, 0, 4, 8, 12, 16, 20, 25), values = (-6, -3, 0, 4, 8, 12, 16, 20))
+        # Set up for larger image.
+        figSize = [12, 6]
+        fig = plt.figure(figsize=figSize, dpi=200)
+        rect = 0,0,200*figSize[0],200*figSize[1]
+        fig.add_axes(rect)
+        geo_axes = plt.axes(projection=ccrs.PlateCarree())
+
         # We need to fix the boundary of the figure (otherwise we get a black border at left & top).
         # Cartopy removes matplotlib's axes.patch (which normally defines the boundary) and
         # replaces it with outline_patch and background_patch.  It's the former which is causing
         # the black border.  Get the axis object and make its outline patch invisible.
-        ax = plt.gca()
-        ax.outline_patch.set_visible(False)
+        geo_axes.outline_patch.set_visible(False)
+        plt.margins(0,0)
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+
+        # Contour plot the temperatures and add the coastline.        
+        iplt.contourf(cube[time], levels = (0.000008, 0.00002, 0.00007, 0.0002, 0.001, 0.05), colors = ('cyan', 'blue', 'yellow', 'orange', 'red'))        
+        
+        #-6.4358826, 27.94899
+        plt.gca().coastlines()
+        #plt.colorbar(boundaries = (-6, -3, 0, 4, 8, 12, 16, 20, 25), values = (-6, -3, 0, 4, 8, 12, 16, 20))
 
         # Extract the year value and display it (coordinates used in locating the text are
         # those of the data).
@@ -75,7 +88,8 @@ def main():
         # Now save the plot in an image file.  The files are numbered sequentially, starting
         # from 000.png; this is so that the ffmpeg command can grok them.
         filename = "rainfall/image-%04d.png" % time
-        plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+#        plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+        plt.savefig(filename, dpi=200)
         
         # Discard the figure (otherwise the text will be overwritten
         # by the next iteration).
